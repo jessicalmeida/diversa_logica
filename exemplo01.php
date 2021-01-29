1d370aee6f4f71087d560f2d734a64917f043c1a
jessicalmeida <43574337+jessicalmeida@users.noreply.github.com>
2021-01-29 15:52:17 -0300

    Create exemplo01.php

---
+++ exemplo01.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Olhando para a direita. \n";
+    echo "Olhando para a esquerda.\n";
+    
+    $temCarro = "sim";
+
+    if($temCarro == "sim")
+    {
+        echo "Atravessei a rua";
+    } else {
+        echo "Não atravessei a rua";
+    }
+?>
