5b8820055174543bf0b5df1dee9bd262c9fdb0a2
jessicalmeida <43574337+jessicalmeida@users.noreply.github.com>
2021-01-31 20:12:09 -0300

    Update tiposDados02.php

--- tiposDados02.php
+++ tiposDados02.php
@@ -1,23 +1,17 @@
 <?php
-    //Número inteiro
     echo 1.1;
     echo "\n";
     
-    //Tipo de dado e o valor
     var_dump(1.0);
     
-    //Inteiro máximo disponível
     echo PHP_FLOAT_MAX;
     echo "\n";
     
-    //Inteiro mínimo disponível
     echo PHP_FLOAT_MIN;
     echo "\n";
     
-    //Número octal
     echo 1.2e3;
     echo "\n";
     
-    //Número binário
     echo 13E-3;
 ?>
